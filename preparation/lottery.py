25c28708453e5cb3a31e6906051c2e90e06cb189
xiaolinzi-xl <daoshangxl@163.com>
2019-11-17 15:49:13 +0800

    抽签（使用二分搜索降低时间复杂度）

--- preparation/lottery.py
+++ preparation/lottery.py
@@ -1,3 +1,4 @@
+# 时间复杂度为 O(N^4)
 def solve(n, m, arr):
     for a in range(n):
         for b in range(n):
@@ -8,6 +9,34 @@ def solve(n, m, arr):
     return False
 
 
+def binary_search(arr, target):
+    l, r = 0, len(arr) - 1
+    while l <= r:
+        mid = (l + r) // 2
+        if arr[mid] == target:
+            return True
+        elif arr[mid] > target:
+            r = mid - 1
+        else:
+            l = mid + 1
+    return False
+
+
+# 时间复杂度为　O(N^2 * logN)
+def solve_1(n, m, arr):
+    record = set()
+    for a in range(n):
+        for b in range(n):
+            record.add(arr[a] + arr[b])
+    record = list(record)
+    record.sort()
+    for a in range(n):
+        for b in range(n):
+            if binary_search(record, m - arr[a] - arr[b]):
+                return True
+    return False
+
+
 if __name__ == '__main__':
     n, m = map(int, input().split())
     arr = list(map(int, input().split()))
@@ -15,3 +44,4 @@ if __name__ == '__main__':
         print('Yes')
     else:
         print('No')
+    print(solve_1(n, m, arr))
